88c63e10331bb86a63250b7cedd3b3956b1ae279
RedwanIfty <ifty8555@gmail.com>
2023-11-12 11:22:00 +0600

    application create

--- app/Http/Controllers/Api/ApiApplicationController.php
+++ app/Http/Controllers/Api/ApiApplicationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\Application;
 use App\Models\Employee;
+use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -27,74 +28,23 @@ class ApiApplicationController extends Controller
         } else {
             $endDate = $request->start;
         }
+        $application = new Application();
+        $application->approval_id=$request->approval_id;
+        $application->employee_id=auth()->user()->id;
+        $application->start_date=$request->start;
+        $application->end_date=$endDate;
+        $application->applied_total_days=$total_days;
+        $application->reason=$request->reason;
+        $application->stay_location=$request->stay_location;
+        $application->leave_type=$request->leave_type_id;
+        $application->status=1;
 
-        $emp = Employee::where('user_id', auth('api')->user()->id)
-            ->leftJoin('designations', 'designations.designation_id', 'employees.designation')
-            ->first();
+        $application->save();
 
-        $leave = Application::where('employee_id', $emp->id)
-            ->where('approved_end_date', '>=', $request->start)
-            ->where('approved_start_date', '<=', $endDate)->first();
-
-        $chkLeavePending = Application::where('employee_id', $emp->id)
-            ->where('start_date', '>=', $request->start)
-            ->where('end_date', '<=', $endDate)
-            ->where('status',1)
-            ->first();
-
-        if ($total_days > 10) {
-
-            return response()->json([
-                'message' => 'সর্বোচ্চ ১০ দিন ছুটি নিতে পারবেন',
-
-            ]);
-
-        }
-
-        if ($chkLeavePending != null) {
-
-            return response()->json([
-                'message' => 'উক্ত তারিখে ছুটির আবেদন পূর্বের ছুটি অপেক্ষামান রয়েছে!',
-
-            ]);
-
-        }
-
-        if ($leave != null) {
-
-            return response()->json([
-                'message' => 'উক্ত তারিখে ছুটির আবেদন পূর্বে গৃহীত করা রয়েছে!',
-
-            ]);
-
-        } else {
-
-
-
-            $application = new Application();
-
-            $application->approval_id = Employee::where('user_id',$request->admin_id)->first()->id;
-
-            $application->employee_id = Employee::where('user_id',auth('api')->user()->id)->first()->id;
-
-
-            $application->reason = $request->reason;
-            $application->stay_location = $request->stay;
-            $application->start_date = $request->start;
-            $application->end_date = $request->end;
-            if ($total_days == 1) {
-                $application->end_date = $request->start;
-            }
-            $application->applied_total_days = $total_days;
-            $application->approved_total_days = 0;
-            $application->status = 1;
-            $application->save();
-
-            return response()->json([
-                'message' => ' Application Submitted Successfully',
-
-            ]);
-        }
+        return response()->json([
+            'message'=>'Application submitted successfully',
+            'application'=>$application
+        ],201);
     }
     public function getLeaveEmployee(){
         try {
